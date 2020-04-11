00a095f40275922248453aef4b4e9ec2d9c123ee
lushi1 <levannamtdmu@gmail.com>
2020-04-11 21:08:49 +0700

    11/04/2020 edit qlkhachsan.blade.php (edit and delete khachsan)

--- resources/views/pages/admin/qlkhachsan.blade.php
+++ resources/views/pages/admin/qlkhachsan.blade.php
@@ -22,75 +22,72 @@
                         
                         <div class="card-body">
                         
-                        <div id="mapid" style="width: 800; height: 600px;"></div>
-                        <script>
+                            <div id="mapid" style="width: 800; height: 500px;"></div>
+                            <script>
 
-                            var mymap = L.map('mapid').setView([10.980692, 106.674437], 13);
+                                var mymap = L.map('mapid').setView([10.980692, 106.674437], 13);
 
-                            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVzaGkiLCJhIjoiY2s0YXFnNHRyMDY2dzNlbGtvM3pwcThhMyJ9.F9DH_aBnwZYWez_5hy3xNA', {
-                                maxZoom: 18,
-                                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                                id: 'mapbox/streets-v11',
-                                tileSize: 512,
-                                zoomOffset: -1
-                            }).addTo(mymap);
-                        </script>                   
-                        <script src="{{asset('/js/geojsondata.js')}}"></script>
-                        <script>                     
-                        var json_KhachSanpoint1={
-                            
-                            "type": "FeatureCollection",
-                            "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                                            
-                            "features": [
-                                @foreach($data as $dt)
-                                    { 
-                                        "type": "Feature", 
-                                        "properties": { 
-                                            "tenkhachsan": "{{$dt->tenkhachsan}}" }, 
-                                            "geometry": { "type": "Point", 
-                                            "coordinates": [ {{$dt->st_x}}, {{$dt->st_y}} ] } },
-                                @endforeach
-                            ]
-                        }
-                        // var myIcon = L.icon({
-                        //     iconUrl: 'hotel.png',
-                        //     iconSize: [25, 40],                    
-                        //     shadowSize: [68, 95],
-                        // });
-
-                        var khachsan = L.geoJson(json_KhachSanpoint1, {  
-                            pointToLayer: function(feature, latlng) {
-                                var smallIcon = new L.Icon({                               
-                                        iconUrl: 'hotel.png',
-                                        iconAnchor: [13, 27],
-                                        iconSize: [27, 27],                    
-                                                              
-                                });
-                                return L.marker(latlng, {icon: smallIcon});
-                            },                       
-                            onEachFeature: function (feature, layer)
-                            {
+                                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHVzaGkiLCJhIjoiY2s0YXFnNHRyMDY2dzNlbGtvM3pwcThhMyJ9.F9DH_aBnwZYWez_5hy3xNA', {
+                                    maxZoom: 18,
+                                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                                        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                                        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                                    id: 'mapbox/streets-v11',
+                                    tileSize: 512,
+                                    zoomOffset: -1
+                                }).addTo(mymap);
+                            </script>                   
+                            <script src="{{asset('/js/geojsondata.js')}}"></script>
+                            <script>                     
+                            var json_KhachSanpoint1={
                                 
-                                layer.bindPopup('<p>Tên khách sạn: ' + feature.properties.tenkhachsan+'</p>');
-                            },
-                                                      
-                        }).addTo(mymap);
-                        var popup = L.popup({minWidth : 200},);
+                                "type": "FeatureCollection",
+                                "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                
+                                "features": [
+                                    @foreach($data as $dt)
+                                        { 
+                                            "type": "Feature", 
+                                            "properties": { 
+                                                "tenkhachsan": "{{$dt->tenkhachsan}}" }, 
+                                                "geometry": {   "type": "Point", 
+                                                                "coordinates": [ {{$dt->st_x}}, {{$dt->st_y}} ], 
+                                                            },
+                                        },
+                                    @endforeach
+                                ]
+                            }                        
+                            var khachsan = L.geoJson(json_KhachSanpoint1, {  
+                                pointToLayer: function(feature, latlng) {
+                                    var smallIcon = new L.Icon({                               
+                                            iconUrl: 'hotel.png',
+                                            iconAnchor: [13, 27],
+                                            iconSize: [27, 27],                    
+                                                                
+                                    });
+                                    return L.marker(latlng, {icon: smallIcon});
+                                },                       
+                                onEachFeature: function (feature, layer)
+                                {     
+                        
+                                    layer.bindPopup('<div class="container-fluid"><div class="form-group"><p>Tên khách sạn: '+feature.properties.tenkhachsan+'</p><p>Tên khách sạn: '+feature.properties.tenkhachsan+'</p></div><div class="row"><div class="col-6"><form action="themKS" method="POST">@csrf<button type="submit" class="btn btn-info">Sửa</button></form></div><div class="col-6"><form action="themKS" method="POST">@csrf<button type="submit" class="btn btn-danger">Xóa</button></form></div></div></div>');
+                                },
+                                                        
+                            }).addTo(mymap);
+                            var popup = L.popup({minWidth : 200},);
 
-                            function onMapClick(e) {
-                                popup                              
-                                    .setLatLng(e.latlng)
-                                    .setContent('<h1><span class="badge badge-dark text-center">Thêm Khách Sạn</span></h1><form action="themKS" method="POST">@csrf<div class="form-group"><label>Tên khách sạn: </label><input type="text" class="form-control" aria-describedby="emailHelp" name="tenkhachsan" placeholder="Vd: Phương Nam" required></div><div class="form-group"><div class="row"><div class="col-6"><label>Tọa độ X: </label><input type="text" class="form-control" value="'+e.latlng.lng+'" name="toadox" readonly></div><div class="col-6"><label>Tọa độ Y: </label><input type="text" class="form-control" name="toadoy" value="'+e.latlng.lat+'" readonly></div></div></div><button type="submit" class="btn btn-primary">Thêm</button>')
-                                    .openOn(mymap);
-                            }
+                                function onMapClick(e) {
+                                    popup                              
+                                        .setLatLng(e.latlng)
+                                        .setContent('<h1><span class="badge badge-dark text-center">Thêm Khách Sạn</span></h1><form action="themKS" method="POST">@csrf<div class="form-group"><label>Tên khách sạn: </label><input type="text" class="form-control" aria-describedby="emailHelp" name="tenkhachsan" placeholder="Vd: Phương Nam" required></div><div class="form-group"><div class="row"><div class="col-6"><label>Tọa độ X: </label><input type="text" class="form-control" value="'+e.latlng.lng+'" name="toadox" readonly></div><div class="col-6"><label>Tọa độ Y: </label><input type="text" class="form-control" name="toadoy" value="'+e.latlng.lat+'" readonly></div></div></div><button type="submit" class="btn btn-primary">Thêm</button>')
+                                        .openOn(mymap);
+                                }
 
-                            mymap.on('click', onMapClick);
-                        </script>
-                        <script src="{{asset('/js/geojson.js')}}"></script>
-
+                                mymap.on('click', onMapClick);
+                            </script>
+                            <script src="{{asset('/js/geojson.js')}}"></script>
+<div class="container-fluid"><div class="form-group"><p>Tên khách sạn: </p><p>Tên khách sạn: </p></div><div class="row"><div class="col-6"><form action="themKS" method="POST">@csrf<button type="submit" class="btn btn-info">Sửa</button></form></div><div class="col-6"><form action="themKS" method="POST">@csrf<button type="submit" class="btn btn-info">Xóa</button></form></div></div></div>
+                            <!-- Ket thuc card body -->
                         </div>
                         <!-- Ket thuc noi dung -->
                         
